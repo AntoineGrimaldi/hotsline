7eb0fdf761497e028206491b286d89fad57f931f
Antoine Grimaldi <antoine.grimaldi@univ-amu.fr>
2022-06-15 14:40:24 +0200

    memory issue with gesture

---
+++ dev/2022_06_14_run_dvsgesture_tau_cla.py
@@ -0,0 +1,80 @@
+import tonic, torch, os
+from hots.network import network
+from hots.utils import apply_jitter, get_loader, get_sliced_loader, make_histogram_classification, HOTS_Dataset, fit_mlr, predict_mlr, score_classif_events
+import numpy as np
+
+print(f'Tonic version installed -> {tonic.__version__}')
+
+print(f'Number of GPU devices available: {torch.cuda.device_count()}')
+for N_gpu in range(torch.cuda.device_count()):
+    print(f'GPU {N_gpu+1} named {torch.cuda.get_device_name(N_gpu)}')
+    
+    
+kfold_test = 2
+kfold_clust = 10
+
+dataset_name = 'gesture'
+slicing_time_window = 1e6
+
+type_transform = tonic.transforms.NumpyAsType(int)
+trainset = tonic.datasets.DVSGesture(save_to='../../Data/', train=True, transform=type_transform)
+testset = tonic.datasets.DVSGesture(save_to='../../Data/', train=False, transform=type_transform)
+loader = get_sliced_loader(trainset, slicing_time_window, dataset_name, True, only_first=True, kfold=kfold_clust)
+trainloader = get_sliced_loader(trainset, slicing_time_window, dataset_name, True, only_first=True, kfold=kfold_test)
+num_sample_train = len(trainloader)
+testloader = get_sliced_loader(testset, slicing_time_window, dataset_name, False, only_first=True, kfold=kfold_test)
+num_sample_test = len(testloader)
+n_classes = len(testset.classes)
+print(f'number of samples in the training set: {len(trainloader)}')
+print(f'number of samples in the testing set: {len(testloader)}')
+
+name = 'homeohots'
+homeo = True
+timestr = '2022-04-22'
+
+Rz = [4, 8]
+N_neuronz = [16, 32]
+tauz = [5e4*2, 5e4*16]
+
+hots = network(name, dataset_name, timestr, trainset.sensor_size, nb_neurons = N_neuronz, tau = tauz, R = Rz, homeo = homeo)
+
+#initial_name = hots.name
+
+filtering_threshold = [2*Rz[L] for L in range(len(Rz))]
+if not os.path.exists('../Records/'):
+    os.mkdir('../Records/')
+    os.mkdir('../Records/networks/')
+path = '../Records/networks/'+hots.name+'.pkl'
+if not os.path.exists(path):
+    hots.clustering(loader, trainset.ordering, filtering_threshold = filtering_threshold)
+    
+jitter = (None, None)
+num_workers = 0
+learning_rate = 0.005
+beta1, beta2 = 0.9, 0.999
+betas = (beta1, beta2)
+num_epochs = 8 #2 ** 5 + 1
+N_output_neurons = N_neuronz[-1]
+ts_size = (trainset.sensor_size[0],trainset.sensor_size[1],N_output_neurons)
+tau_cla_list = [1e5, 2e5, 3e5, 5e5, 1e6, 2e6]
+
+train_path = f'../Records/output/train/{hots.name}_{num_sample_train}_{jitter}/'
+test_path = f'../Records/output/test/{hots.name}_{num_sample_test}_{jitter}/'
+
+hots.coding(trainloader, trainset.ordering, trainset.classes, training=True, verbose=False)
+hots.coding(testloader, testset.ordering, testset.classes, training=False, verbose=False)
+
+trainset_output = HOTS_Dataset(train_path, trainset.sensor_size, trainset.classes, dtype=trainset.dtype, transform=type_transform)
+trainoutputloader = get_loader(trainset_output)
+testset_output = HOTS_Dataset(test_path, testset.sensor_size, testset.classes, dtype=testset.dtype, transform=type_transform)
+testoutputloader = get_loader(testset_output)
+
+for tau_cla in tau_cla_list:
+    
+    model_path = f'../Records/networks/{hots.name}_{tau_cla}_{learning_rate}_{betas}_{num_epochs}_{jitter}.pkl'
+    results_path = f'../Records/LR_results/{hots.name}_{tau_cla}_{learning_rate}_{betas}_{num_epochs}_{jitter}.pkl'
+    
+    classif_layer, losses = fit_mlr(trainoutputloader, model_path, tau_cla, learning_rate, betas, num_epochs, ts_size, trainset.ordering, len(trainset.classes))
+    likelihood, true_target, timestamps = predict_mlr(classif_layer,tau_cla,testoutputloader,results_path,ts_size,testset_output.ordering)
+    meanac, onlinac, lastac = score_classif_events(likelihood, true_target, n_classes, original_accuracy = score, original_accuracy_nohomeo = score_nohomeo, figure_name = 'nmnist_online.pdf')
+    print(f'For tau = {tau} last accuracy: {lastac*100}% - mean accuracy: {meanac*100}%')
