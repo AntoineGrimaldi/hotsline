ff489f41162f803d2d664184d4c2b63a5d8c9a5a
Antoine Grimaldi <antoine.grimaldi@univ-amu.fr>
2022-07-22 07:44:01 +0200

    run nmist

--- dev/2022-07-21_run_nmnist_jitter.py
+++ dev/2022-07-21_run_nmnist_jitter.py
@@ -9,9 +9,9 @@ print(f'Number of GPU devices available: {torch.cuda.device_count()}')
 for N_gpu in range(torch.cuda.device_count()):
     print(f'GPU {N_gpu+1} named {torch.cuda.get_device_name(N_gpu)}')
     
-device = "cpu"
+device = "cuda"
     
-drop_proba_mlr = None
+drop_proba_mlr = .1
 kfold = None
 
 type_transform = tonic.transforms.NumpyAsType(int)
