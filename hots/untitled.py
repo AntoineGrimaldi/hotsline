ca60190cdae1a2d3a9f1c54eadc350cd26bdf1c8
Antoine Grimaldi <antoine.grimaldi@univ-amu.fr>
2023-02-21 15:10:43 +0100

    big dvs learning

--- hots/untitled.py
+++ hots/untitled.py
@@ -14,12 +14,14 @@ for N_gpu in range(torch.cuda.device_count()):
     print(f'GPU {N_gpu+1} named {torch.cuda.get_device_name(N_gpu)}')
 
 record_path = '../Records/'
+device = 'cuda'
 
 kfold_clust = 10
 kfold_test = None
 
 dataset_name = 'gesture'
 slicing_time_window = 1e6
+ts_batch_size = 10000
 
 type_transform = tonic.transforms.NumpyAsType(int)
 trainset = tonic.datasets.DVSGesture(save_to='../../Data/', train=True, transform=type_transform)
@@ -33,16 +35,15 @@ n_classes = len(testset.classes)
 print(f'number of samples in the training set: {len(trainloader)}')
 print(f'number of samples in the testing set: {len(testloader)}')
 
-
 name = 'homeohots'
 homeo = True
-timestr = '2022-12-01'
+timestr = '2023-01-04'
 dataset_name = 'gesture'
 
 Rz = [4, 8]
-N_neuronz = [16, 32]
+N_neuronz = [32, 64]
 layer_threshold = [0.05, 0.1]
-tauz = [3e3*2, 3e3*16]
+tauz = [5e3*2, 5e3*32]
 
 hots = network(name, dataset_name, timestr, trainset.sensor_size, nb_neurons = N_neuronz, tau = tauz, R = Rz, homeo = homeo, record_path=record_path)
 
@@ -54,13 +55,13 @@ if not os.path.exists(record_path):
     os.mkdir(record_path+'networks/')
 path = record_path+'networks/'+hots.name+'.pkl'
 if not os.path.exists(path):
-    hots.clustering(loader, trainset.ordering, filtering_threshold = filtering_threshold, device = 'cuda')
-    
+    hots.clustering(loader, trainset.ordering, filtering_threshold = filtering_threshold, ts_batch_size = ts_batch_size, device = device)
     
 jitter = (None, None)
 
-hots.coding(trainloader, trainset.ordering, trainset.classes, filtering_threshold = filtering_threshold, layer_threshold = layer_threshold, training=True, verbose=False)
-hots.coding(testloader, trainset.ordering, trainset.classes, filtering_threshold = filtering_threshold, layer_threshold = layer_threshold, training=False, verbose=False)
+hots.coding(trainloader, trainset.ordering, trainset.classes, filtering_threshold = filtering_threshold, layer_threshold = layer_threshold, training=True, ts_batch_size = ts_batch_size, verbose=False, device = device)
+hots.coding(testloader, trainset.ordering, trainset.classes, filtering_threshold = filtering_threshold, layer_threshold = layer_threshold, training=False, ts_batch_size = ts_batch_size, verbose=False, device = device)
+
 
 num_workers = 0
 learning_rate = 0.00005
@@ -69,10 +70,10 @@ betas = (beta1, beta2)
 num_epochs = 2 ** 5 + 1
 N_output_neurons = N_neuronz[-1]
 ts_size = (trainset.sensor_size[0],trainset.sensor_size[1],N_output_neurons)
-tau_cla = 3e4*32
+tau_cla = 5e3*64
 drop_proba = .95
 
-ts_batch_size = 1000
+ts_batch_size = 300
 
 train_path = f'../Records/output/train/{hots.name}_{num_sample_train}_{jitter}/'
 test_path = f'../Records/output/test/{hots.name}_{num_sample_test}_{jitter}/'
@@ -87,6 +88,9 @@ trainset_output = HOTS_Dataset(train_path, trainset.sensor_size, trainset.classe
 trainoutputloader = get_loader(trainset_output, kfold = kfold_mlr)
 testset_output = HOTS_Dataset(test_path, testset.sensor_size, testset.classes, dtype=testset.dtype, transform=type_transform)
 testoutputloader = get_loader(testset_output)
+
+score = make_histogram_classification(trainset_output, testset_output, N_output_neurons)
+print(f'Histogram classification performance: {np.round(score,3)*100} %')
 
 classif_layer, losses = fit_mlr(trainoutputloader, model_path, tau_cla, learning_rate, betas, num_epochs, ts_size, trainset.ordering, len(trainset.classes), ts_batch_size = ts_batch_size, drop_proba = drop_proba)
 
